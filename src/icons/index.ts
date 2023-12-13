52c55864837f36beedd2bdf144ef0b7e759a2a3b
Agata Vitoria <agatavitoriadelima@gmail.com>
2023-12-12 23:01:47 -0300

    feat: added icons

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as ArrowDownIcon } from "./ArrowDownIcon";
+export { default as BoldIcon } from "./BoldIcon";
+export { default as ItalicIcon } from "./ItalicIcon";
+export { default as UnderlineIcon } from "./UnderlineIcon";
